6ed05654a08769bb9c7dab16c5515a0fd4f9bff3
turgunbaevaa <nyc.aruu@gmail.com>
2024-01-21 08:48:25 +0100

    Initial Commit

---
+++ ContactsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsApp
+//
+//  Created by Aruuke Turgunbaeva on 21/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
